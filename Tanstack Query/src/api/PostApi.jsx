05a03e9e8fe6aaa3faf576e2345310ddc744979d
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-07-11 03:46:05 +0500

    Practice patch delete method and create infinitescroll api

--- Tanstack Query/src/api/PostApi.jsx
+++ Tanstack Query/src/api/PostApi.jsx
@@ -14,6 +14,14 @@ export const PostApi = async (pageNumber) => {
   }
 };
 
+export const delPost = (id) => {
+  return api.delete(`/posts/${id}`)
+}
+
+export const updatePost = (id) => {
+  return api.patch(`/posts/${id}`, { title: 'I have updated' })
+}
+
 export const fetchIndvPost = async (id) => {
   try {
     const res = await api.get(`/posts/${id}`);
@@ -23,3 +31,20 @@ export const fetchIndvPost = async (id) => {
     throw new Error('Failed to fetch posts');
   }
 }
+
+const userApi = axios.create({
+  baseURL: 'https://api.github.com',
+});
+
+export const fetchUsers = async ({ pageParam = 1 }) => {
+  try {
+    const res = await userApi.get(`/users?per_page=10&page=${pageParam}`);
+    return {
+      users: res.data,
+      nextPage: res.data.length > 0 ? pageParam + 1 : undefined, // Stop if no more users
+    };
+  } catch (error) {
+    console.error('API Error:', error.message);
+    throw new Error('Failed to fetch users'); // Fixed error message
+  }
+};
